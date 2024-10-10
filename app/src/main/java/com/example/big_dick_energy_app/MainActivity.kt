c30632299303a0a6bb0bea5cd26e796cba6da048
pavlicek-cpu <yolokappa@email.cz>
2024-10-10 18:20:48 +0200

    big dick energy !!!!

--- app/src/main/java/com/example/big_dick_energy_app/MainActivity.kt
+++ app/src/main/java/com/example/big_dick_energy_app/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello you have a really BIG DICK energy!!!!!",
         modifier = modifier
     )
 }
